8f64161d6420c9adac50134d941ea5db6e67b186
margawron <max2.marcel@gmail.com>
2019-12-22 20:59:15 +0100

    TODO: Add pixel history functionality

--- src/main/java/pl/polsl/gawron/marcel/rplace/models/User.java
+++ src/main/java/pl/polsl/gawron/marcel/rplace/models/User.java
@@ -1,5 +1,8 @@
 package pl.polsl.gawron.marcel.rplace.models;
 
+import java.util.List;
+import java.util.Random;
+
 /**
  * Class representing single user of an application
  *
@@ -10,8 +13,34 @@ public class User {
     private Long id;
     private String name;
     private String password;
-    //private List<Pixel>
+    private List<PixelHistory> userHistory;
 
+    /**
+     * Default class constructor
+     * randomizes Id
+     * sets name as undefined
+     */
     public User() {
+        this.id = new Random().nextLong();
+        this.name = "undefined";
     }
+
+    /**
+     * Constructor with name parameter
+     *
+     * @param name name to be set as member value name
+     */
+    public User(String name){
+        this();
+        setName(name);
+    }
+
+    /**
+     * Setter for the name property
+     * @param name new name to be set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
