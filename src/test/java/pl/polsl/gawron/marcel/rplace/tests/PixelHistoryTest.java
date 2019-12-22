dbe674391613b78c1d256d6ec2cea21c68070cd0
margawron <max2.marcel@gmail.com>
2019-12-22 22:55:10 +0100

    Testing simple POJOs, without database is not necessary.

--- src/test/java/pl/polsl/gawron/marcel/rplace/tests/PixelHistoryTest.java
+++
@@ -1,6 +0,0 @@
-package pl.polsl.gawron.marcel.rplace.tests;
-
-public class PixelHistoryTest {
-    // TODO
-    // NEED TO IMPLEMENT PIXELHISTORY CLASS
-}
--- src/test/java/pl/polsl/gawron/marcel/rplace/tests/UserTest.java
+++
@@ -1,4 +0,0 @@
-package pl.polsl.gawron.marcel.rplace.tests;
-
-public class UserTest {
-}
