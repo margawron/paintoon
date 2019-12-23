552be2722c23f99cd0984b6e2278bd1b4e71252c
margawron <max2.marcel@gmail.com>
2019-12-23 01:14:49 +0100

    Started to implement server application.

--- rplace-server/src/main/java/pl/polsl/gawron/marcel/rplace/Main.java
+++ rplace-server/src/main/java/pl/polsl/gawron/marcel/rplace/Main.java
@@ -1,9 +1,13 @@
 package pl.polsl.gawron.marcel.rplace;
 
-import pl.polsl.gawron.marcel.rplace.controllers.ImageController;
-import pl.polsl.gawron.marcel.rplace.models.Color;
-import pl.polsl.gawron.marcel.rplace.models.Image;
-import pl.polsl.gawron.marcel.rplace.utils.ArgumentParser;
+import pl.polsl.gawron.marcel.rplace.controllers.ProtocolController;
+import pl.polsl.gawron.marcel.rplace.services.ServerService;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
 
 /**
  * Main class of a program
@@ -11,23 +15,62 @@ import pl.polsl.gawron.marcel.rplace.utils.ArgumentParser;
  * @author Marcel Gawron
  * @version 1.0
  */
-public class Main {
+public class Main implements Closeable {
+    /**
+     * Main class of server application
+     * @param args
+     */
+    // Network
+    final private int PORT = 9876;
+    private ServerSocket serverSocket;
+    private ProtocolController protocolController;
     public static void main(String[] args) {
-        // Argument parser for program
-        ArgumentParser argparse = new ArgumentParser(args);
-        argparse.parse();
-        argparse.printConfig();
+        try{
+            Main server = new Main();
+            server.startServer();
+        } catch (IOException e){
 
-        // Creates an Image
-        ImageController bmpCtrl = new ImageController(new Image(100));
-        for (int i = 0; i < 100; ++i) {
-            bmpCtrl.setPixel(i, i, (byte) -128, (byte) -128, (byte) 0);
         }
-        for (int i = 0; i < 100; ++i) {
-            bmpCtrl.setPixel(99 - i, i, new Color(0, 0, 255));
+    }
+
+    public Main(){
+        protocolController = new ProtocolController();
+    }
+
+    public void startServer() throws IOException {
+        serverSocket = new ServerSocket(PORT);
+        System.out.println("Server started");
+        while (true){
+            Socket socket = serverSocket.accept();
+            try(ServerService serverService = new ServerService(socket)){
+                serverService.respond(protocolController);
+            } catch (IOException e){
+                System.err.println(e.getMessage());
+            }
         }
-        bmpCtrl.saveAsFile("test");
+    }
 
-
+    @Override
+    public void close() throws IOException {
+        if (serverSocket != null){
+            serverSocket.close();
+        }
     }
 }
+
+/**
+ *         // Argument parser for program
+ *         ArgumentParser argparse = new ArgumentParser(args);
+ *         argparse.parse();
+ *         argparse.printConfig();
+ *
+ *         // Creates an Image
+ *         ImageController bmpCtrl = new ImageController(new Image(100));
+ *         for (int i = 0; i < 100; ++i) {
+ *             bmpCtrl.setPixel(i, i, (byte) -128, (byte) -128, (byte) 0);
+ *         }
+ *         for (int i = 0; i < 100; ++i) {
+ *             bmpCtrl.setPixel(99 - i, i, new Color(0, 0, 255));
+ *         }
+ *         bmpCtrl.saveAsFile("test");
+ */
---
+++ rplace-server/src/main/java/pl/polsl/gawron/marcel/rplace/services/ServerService.java
@@ -0,0 +1,44 @@
+package pl.polsl.gawron.marcel.rplace.services;
+
+import pl.polsl.gawron.marcel.rplace.controllers.ProtocolController;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ServerService implements Closeable {
+
+    private Socket socket;
+    private BufferedReader input;
+    private PrintWriter output;
+
+    public ServerService(Socket socket) throws IOException{
+        this.socket = socket;
+        output = new PrintWriter(new BufferedWriter(
+                new OutputStreamWriter(socket.getOutputStream())),true);
+        input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+    }
+
+    public void respond(ProtocolController controller){
+        try {
+            while (true){
+                controller.processRequest(input,output);
+            }
+        } catch (IOException e){
+            System.err.println(e.getMessage());
+        } finally {
+            try{
+                socket.close();
+            } catch (IOException e){
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        if(socket != null){
+            socket.close();
+        }
+    }
+}
