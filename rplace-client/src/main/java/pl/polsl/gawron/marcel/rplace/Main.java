716d6edb263512e236671b626830bb7094b79160
margawron <max2.marcel@gmail.com>
2019-12-23 01:11:41 +0100

    Javadoc for Client application.

--- rplace-client/src/main/java/pl/polsl/gawron/marcel/rplace/Main.java
+++ rplace-client/src/main/java/pl/polsl/gawron/marcel/rplace/Main.java
@@ -1,8 +1,17 @@
 package pl.polsl.gawron.marcel.rplace;
 
+/**
+ * Client side main function
+ *
+ * @author Marcel Gawron
+ * @version 1.0
+ */
 public class Main {
-
-
+    /**
+     * Main function of a client side application
+     *
+     * @param args arguments passed to client program
+     */
     public static void main(String[] args) {
 
     }
