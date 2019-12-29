6f7a633426005756f0bb70a99c0f72160bd16a2a
margawron <max2.marcel@gmail.com>
2019-12-29 01:35:49 +0100

    Configurable Image size.

---
+++ rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/config/ImageSizeConfig.java
@@ -0,0 +1,18 @@
+package pl.polsl.gawron.marcel.rplaceServer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pl.polsl.gawron.marcel.rplaceData.models.Image;
+
+/**
+ * @author Marcel Gawron
+ * @version 1.0
+ */
+@Configuration
+public class ImageSizeConfig {
+
+    @Bean
+    public Image setImageSize() {
+        return new Image(1000);
+    }
+}
