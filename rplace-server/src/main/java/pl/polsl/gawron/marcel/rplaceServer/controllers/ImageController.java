1ff69821ec0846cba5ccd56833b6edbc4d8860f3
margawron <max2.marcel@gmail.com>
2019-12-29 01:36:38 +0100

    Image possible to view @ localhost:8080/image || your.domain/image

---
+++ rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/controllers/ImageController.java
@@ -0,0 +1,43 @@
+package pl.polsl.gawron.marcel.rplaceServer.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.polsl.gawron.marcel.rplaceData.models.Image;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.Raster;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * @author Marcel Gawron
+ * @version 1.0
+ */
+@Controller
+public class ImageController {
+
+    private Image image;
+    private BufferedImage bufferedImage;
+
+    public ImageController(Image image) {
+        this.image = image;
+        bufferedImage = new BufferedImage(image.getSize(), image.getSize(), BufferedImage.TYPE_3BYTE_BGR);
+    }
+
+    @RequestMapping(value = "/image", method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
+    public @ResponseBody
+    byte[] getImage() throws IOException {
+        image.getBitmap();
+        bufferedImage.setData(Raster.createRaster(bufferedImage.getSampleModel(), new DataBufferByte(image.getBitmap(), 3 * image.getSize() * image.getSize()), new Point()));
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, "png", byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+}
