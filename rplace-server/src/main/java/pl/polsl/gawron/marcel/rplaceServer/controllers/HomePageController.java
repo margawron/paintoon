e1946c0ca0ab3867238f5256a3606edc3f1a41d6
margawron <max2.marcel@gmail.com>
2020-01-02 02:13:02 +0100

    Model not needed for home page /* yet */.

--- rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/controllers/HomePageController.java
+++ rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/controllers/HomePageController.java
@@ -1,24 +1,35 @@
 package pl.polsl.gawron.marcel.rplaceServer.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * TODO:
+ * https://dzone.com/articles/how-to-use-cookies-in-spring-boot
+ */
 
 /**
  * Main web application controller
- *
  * @author Marcel Gawron
  * @version 1.0
  */
 @Controller
 public class HomePageController {
 
+    /**
+     * Default constructor
+     */
     public HomePageController() {
 
     }
 
+    /**
+     * Function responsible for responding with homepage
+     * @return name of the home page template
+     */
     @RequestMapping({"", "/", "index"})
-    public String homepage(Model model) {
+    public String homepage() {
 
         return "homepage";
     }
