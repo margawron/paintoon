6cce08aef7d8ca178c2ff5ec51695c3a9333c69c
margawron <max2.marcel@gmail.com>
2020-01-03 03:16:55 +0100

    Check cookies before login and registration. Added stats page.

--- rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/controllers/LoginController.java
+++ rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/controllers/LoginController.java
@@ -2,6 +2,7 @@ package pl.polsl.gawron.marcel.rplaceServer.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +11,7 @@ import pl.polsl.gawron.marcel.rplaceServer.models.LoginFormModel;
 import pl.polsl.gawron.marcel.rplaceServer.repositories.UserRepository;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -23,44 +25,87 @@ public class LoginController {
 
     /**
      * Constructor for login controller
+     *
      * @param userRepository repository handling user data/database
      */
-    public LoginController(UserRepository userRepository){
+    public LoginController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     /**
+     * Responds to HTTP GET requests with stats page
+     *
+     * @param model object containing attributes for rendering views
+     * @return name of the template to render
+     */
+    @RequestMapping(path = "/loggedIn", method = RequestMethod.GET)
+    public String loggedIn(Model model,
+                           @CookieValue(name = "username", required = false) String username,
+                           @CookieValue(name = "token", required = false) String token) {
+        // Check for lost souls
+        if(username == null || token == null){
+            return "redirect:/login";
+        }
+
+
+        return "loggedInPage";
+    }
+
+    /**
      * Responds to HTTP GET requests with login page
+     *
      * @param model object containing attributes for rendering views
      * @return name of the template to render
      */
     @RequestMapping(path = "/login", method = RequestMethod.GET)
-    public String sendLoginPage(Model model) {
+    public String sendLoginPage(Model model,
+                                HttpServletRequest request) {
+
+        // Get necessary cookies
+        Cookie[] cookies = request.getCookies();
+        String username = null;
+        String token = null;
+        for (var cookie : cookies) {
+            if (cookie.getName().equals("username")) {
+                username = cookie.getValue();
+            }
+            if (cookie.getName().equals("token")) {
+                token = cookie.getValue();
+            }
+        }
+        // Check if user is already logged and if he is, redirect him to login page
+        if (username != null && token != null) {
+            User user = userRepository.findUser(username);
+            if (user != null && user.isTokenValid(token)) {
+                return "redirect:/loggedIn";
+            }
+        }
+
         model.addAttribute("formModel", new LoginFormModel());
         return "login";
     }
 
     /**
      * Responds to HTTP POST requests from register page
+     *
      * @param formModel data model of a form in a register page
-     * @param model model object containing attributes for rendering views
+     * @param model     model object containing attributes for rendering views
      * @return name of the template to render
      */
     @RequestMapping(path = "/login", method = RequestMethod.POST)
-    public String loginUser(@ModelAttribute LoginFormModel formModel, Model model, HttpServletResponse response){
+    public String loginUser(@ModelAttribute LoginFormModel formModel, Model model, HttpServletResponse response) {
         int nameLength = formModel.getName().length();
         model.addAttribute("shouldRedirectToRegister", false);
-        if(nameLength < 3 || nameLength > 25)
-        {
+        if (nameLength < 3 || nameLength > 25) {
             model.addAttribute("message", "Username is not valid length");
             return "authMessage";
         }
         User user = userRepository.findUser(formModel.getName());
-        if(user == null){
+        if (user == null) {
             model.addAttribute("message", "User does not exist");
             return "authMessage";
         }
-        if(!user.getPassword().equals(formModel.getPassword())){
+        if (!user.getPassword().equals(formModel.getPassword())) {
             model.addAttribute("message", "Password does not match");
             return "authMessage";
         }
@@ -77,7 +122,7 @@ public class LoginController {
 
         response.addCookie(usernameCookie);
         response.addCookie(tokenCookie);
-        return "loggedInPage";
+        return "redirect:/loggedIn";
     }
 
 }
--- rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/controllers/RegisterController.java
+++ rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/controllers/RegisterController.java
@@ -5,8 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pl.polsl.gawron.marcel.rplaceData.models.User;
 import pl.polsl.gawron.marcel.rplaceServer.models.RegistrationFormModel;
 import pl.polsl.gawron.marcel.rplaceServer.repositories.UserRepository;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Controller responsible for registering users
@@ -21,6 +25,7 @@ public class RegisterController {
 
     /**
      * Constructor for register controller
+     *
      * @param userRepository repository handling user data/database
      */
     public RegisterController(UserRepository userRepository) {
@@ -29,25 +34,46 @@ public class RegisterController {
 
     /**
      * Responds to HTTP GET requests with register page
+     *
      * @param model model object containing attributes for rendering views
      * @return name of the template to render
      */
     @RequestMapping(path = "/register", method = RequestMethod.GET)
-    public String sendRegisterPage(Model model) {
-        model.addAttribute("formModel", new RegistrationFormModel());
+    public String sendRegisterPage(Model model,
+                                   HttpServletRequest request) {
+        // Get necessary cookies
+        Cookie[] cookies = request.getCookies();
+        String username = null;
+        String token = null;
+        for (var cookie : cookies) {
+            if (cookie.getName().equals("username")) {
+                username = cookie.getValue();
+            }
+            if (cookie.getName().equals("token")) {
+                token = cookie.getValue();
+            }
+        }
+        // Check if user is already logged and if he is, redirect him to login page
+        if (username != null && token != null) {
+            User user = userRepository.findUser(username);
+            if (user != null && user.isTokenValid(token)) {
+                return "redirect:/loggedIn";
+            }
+        }
 
+        model.addAttribute("formModel", new RegistrationFormModel());
         return "register";
     }
 
     /**
      * Responds to HTTP POST requests from register page
+     *
      * @param formModel data model of a form in a register page
-     * @param model model object containing attributes for rendering views
+     * @param model     model object containing attributes for rendering views
      * @return name of the template to render
      */
     @RequestMapping(path = "/register", method = RequestMethod.POST)
-    public String userRegister(@ModelAttribute RegistrationFormModel formModel, Model model)
-    {
+    public String userRegister(@ModelAttribute RegistrationFormModel formModel, Model model) {
         model.addAttribute("shouldRedirectToRegister", true); // Show register page button
 
         if (!formModel.getName().matches("(?!.*[.\\-_]{2,})^[a-zA-Z0-9.\\-_]{3,24}$")) {
@@ -55,11 +81,11 @@ public class RegisterController {
                     "Username should be 3-24 characters long, can contain letters, numbers and special characters ._-(which cannot be consecutive or combined)");
             return "authMessage";
         }
-        if (!formModel.getPasswordFirst().matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,20}$")){
+        if (!formModel.getPasswordFirst().matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,20}$")) {
             model.addAttribute("message", "Password should be 8-20 characters long and have at least 1 uppercase letter, 1 lower case letter, and 1 number");
             return "authMessage";
         }
-        if(!formModel.getPasswordFirst().equals(formModel.getPasswordSecond())){
+        if (!formModel.getPasswordFirst().equals(formModel.getPasswordSecond())) {
             model.addAttribute("message", "Passwords dont match");
             return "authMessage";
         }
