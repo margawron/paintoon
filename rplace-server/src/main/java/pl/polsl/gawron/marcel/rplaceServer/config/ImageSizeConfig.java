84e068f435a72f708f9c1ea25b3add6cc9252ff4
margawron <max2.marcel@gmail.com>
2020-01-04 01:26:08 +0100

    Javadoc for ImageSizeConfig

--- rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/config/ImageSizeConfig.java
+++ rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/config/ImageSizeConfig.java
@@ -5,12 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import pl.polsl.gawron.marcel.rplaceData.models.Image;
 
 /**
+ * Spring config
+ *
  * @author Marcel Gawron
  * @version 1.0
  */
 @Configuration
 public class ImageSizeConfig {
 
+    /**
+     * Proper constructor needs to be provided for Spring to know
+     * how to construct Image model
+     * @return Image object (size of cavnas is 700x700)
+     */
     @Bean
     public Image setImageSize() {
         return new Image(700);
