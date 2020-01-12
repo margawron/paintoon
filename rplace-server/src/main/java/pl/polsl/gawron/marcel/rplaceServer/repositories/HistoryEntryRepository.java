13073b188913a2da66ed035d06d27bcc846ea2ea
margawron <max2.marcel@gmail.com>
2020-01-12 23:39:09 +0100

    Reimplemented Repository classes to use JDBC connection.

--- rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/repositories/HistoryEntryRepository.java
+++ rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/repositories/HistoryEntryRepository.java
@@ -1,9 +1,21 @@
 package pl.polsl.gawron.marcel.rplaceServer.repositories;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
+import pl.polsl.gawron.marcel.rplaceData.models.Color;
 import pl.polsl.gawron.marcel.rplaceData.models.HistoryEntry;
 
+import java.sql.JDBCType;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,23 +26,40 @@ import java.util.Map;
  */
 @Component
 public class HistoryEntryRepository {
-    private Map<Integer, HistoryEntry> historyEntries;
+
+    private JdbcTemplate template;
+    private UserRepository userRepository;
 
     /**
      * Default constructor
      * initializes member objects
      */
-    public HistoryEntryRepository() {
-        historyEntries = new HashMap();
+    public HistoryEntryRepository(JdbcTemplate template, UserRepository userRepository) {
+        this.template = template;
+        this.userRepository = userRepository;
+        template.execute("DROP TABLE IF EXISTS historyEntries");
+        template.execute("CREATE TABLE historyEntries(" +
+                "id INTEGER AUTO_INCREMENT PRIMARY KEY," +
+                "x INTEGER NOT NULL," +
+                "y INTEGER NOT NULL," +
+                "red INTEGER NOT NULL," +
+                "green INTEGER NOT NULL," +
+                "blue INTEGER NOT NULL," +
+                "userId INTEGER NOT NULL ," +
+                "timeOfModification INTEGER NOT NULL)");
     }
 
     /**
-     * Adds history entry
+     * Adds history entry to database
      *
      * @param entry history entry to be added
      */
     public void addHistoryEntry(HistoryEntry entry) {
-        historyEntries.put(historyEntries.size(), entry);
+        template.update("INSERT INTO historyEntries(x,y,red,green,blue,userId,timeOfModification)" +
+                " VALUES (?,?,?,?,?,?,?)",
+                entry.getX(),entry.getY(),
+                entry.getColor().getRed(),entry.getColor().getGreen(), entry.getColor().getBlue(),
+                entry.getUserWhoModifiedPixel().getId(), entry.getTimeOfModification().atZone(ZoneId.systemDefault()).toEpochSecond());
     }
 
     /**
@@ -40,6 +69,40 @@ public class HistoryEntryRepository {
      * @return history entry
      */
     public HistoryEntry getHistoryEntry(int id) {
-        return historyEntries.get(id);
+        HistoryEntry historyEntry = null;
+        try {
+            historyEntry = template.queryForObject("SELECT * FROM historyEntries WHERE id = ?", new Object[]{id}, new HistoryEntryRowMapper() );
+        } catch (EmptyResultDataAccessException e){
+            historyEntry = null;
+        }
+        return historyEntry;
+    }
+
+    public List<HistoryEntry> getHistoryEntries(){
+        List<HistoryEntry> historyEntries = null;
+        try {
+            historyEntries = template.query("SELECT * FROM historyEntries", new HistoryEntryRowMapper());
+        }catch (EmptyResultDataAccessException e){
+            historyEntries = null;
+        }
+        return historyEntries;
+    }
+
+    public final class HistoryEntryRowMapper implements RowMapper<HistoryEntry> {
+        @Override
+        public HistoryEntry mapRow(ResultSet resultSet, int i) throws SQLException {
+            HistoryEntry historyEntry = new HistoryEntry();
+            historyEntry.setId(resultSet.getLong("id"));
+            historyEntry.setX(resultSet.getInt("x"));
+            historyEntry.setY(resultSet.getInt("y"));
+            historyEntry.setColor(new Color(
+                    resultSet.getInt("red"),
+                    resultSet.getInt("green"),
+                    resultSet.getInt("blue")
+            ));
+            historyEntry.setUserWhoModifiedPixel(userRepository.findUser(resultSet.getInt("userId")));
+            historyEntry.setTimeOfModification(LocalDateTime.ofEpochSecond(resultSet.getInt("timeOfModification"),0, ZoneOffset.UTC));
+            return historyEntry;
+        }
     }
 }
--- rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/repositories/UserRepository.java
+++ rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/repositories/UserRepository.java
@@ -1,29 +1,42 @@
 package pl.polsl.gawron.marcel.rplaceServer.repositories;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import pl.polsl.gawron.marcel.rplaceData.models.User;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.List;
 
 
 /**
- * In-memory database of registered users
+ * Database handler for registered users
  *
  * @author Marcel Gawron
  * @version 1.0
  */
 @Component
 public class UserRepository {
-    private Set<User> users;
+    private JdbcTemplate template;
 
     /**
      * Default constructor
-     * initializes Set of users
+     * Deletes old table and creates a new one
      */
-    public UserRepository() {
-        users = new HashSet<>();
+    public UserRepository(JdbcTemplate template) {
+        this.template = template;
+        template.execute("DROP TABLE IF EXISTS users");
+        template.execute("CREATE TABLE users(" +
+                "id INTEGER AUTO_INCREMENT PRIMARY KEY," +
+                "name TEXT NOT NULL," +
+                "password TEXT NOT NULL," +
+                "tokenExpiryServerTime INTEGER," +
+                "token TEXT)");
     }
 
     /**
@@ -37,14 +50,7 @@ public class UserRepository {
         if (findUser(name) != null) {
             return false;
         } else {
-            User newUser = new User();
-            newUser.setId((long) users.size());
-            newUser.setName(name);
-            newUser.setPassword(password);
-            newUser.setToken(null);
-            newUser.setTokenExpiryServerTime(null);
-            newUser.setUserPixelModificationHistory(new ArrayList<>(10));
-            users.add(newUser);
+            template.update("INSERT INTO users(name, password) VALUES (?,?)",name,password);
             return true;
         }
     }
@@ -56,13 +62,66 @@ public class UserRepository {
      * @return null if user was not found, user instance otherwise
      */
     public User findUser(String name) {
-        User soughtUser = null;
-        for (User user : users) {
-            if (user.getName().equals(name)) {
-                soughtUser = user;
-                break;
+        User toReturn;
+        try {
+             toReturn = template.queryForObject("Select * from users where name = ?", new Object[]{name}, new UserRowMapper());
+        }
+        catch(EmptyResultDataAccessException e){
+            toReturn = null;
+        }
+        return toReturn;
+    }
+
+    public User findUser(int id) {
+        User toReturn;
+        try {
+            toReturn = template.queryForObject("Select * from users where id = ?", new Object[]{id}, new UserRowMapper());
+        }
+        catch(EmptyResultDataAccessException e){
+            toReturn = null;
+        }
+        return toReturn;
+    }
+
+    /**
+     * Updates user entry in the database
+     * @param user user entry
+     */
+    public void updateUser(User user){
+        template.update("UPDATE users SET name = ?, password = ?, token = ?, tokenExpiryServerTime = ? WHERE id = ?",
+                user.getName(), user.getPassword(),user.getToken(), user.getTokenExpiryServerTime().atZone(ZoneId.systemDefault()).toEpochSecond(),user.getId());
+    }
+
+    /**
+     * Returns list of users from database
+     * @return list of users
+     */
+    public List<User> getAllUsers(){
+        List<User> toReturn = null;
+        try {
+            toReturn = template.query("SELECT * FROM users", new UserRowMapper());
+        }catch (EmptyResultDataAccessException ignored){
+        }
+        return toReturn;
+    }
+
+    /**
+     * Class implementing interaface of a row mapper
+     * for User class
+     * @author Marcel Gawron
+     * @version 1.0
+     */
+    public final class UserRowMapper implements RowMapper<User> {
+        @Override
+        public User mapRow(ResultSet resultSet, int i) throws SQLException {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setPassword(resultSet.getString("password"));
+                user.setToken(resultSet.getString("token"));
+                user.setTokenExpiryServerTime(LocalDateTime.ofEpochSecond(resultSet.getInt("tokenExpiryServerTime"), 0, ZoneOffset.UTC));
+                return user;
             }
         }
-        return soughtUser;
     }
-}
+
