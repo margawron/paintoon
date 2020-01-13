2f40147a5cdf3d1f9f7a50af5a762f9527c2c818
margawron <max2.marcel@gmail.com>
2020-01-13 01:11:54 +0100

    Add javadoc to ImageSizeConfig.

--- rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/config/ImageSizeConfig.java
+++ rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/config/ImageSizeConfig.java
@@ -36,19 +36,35 @@ public class ImageSizeConfig {
      * Insert latest set pixels for every pixel in bitmap
      * @param image bitmap object
      * @param historyEntryRepository database access class
-     * @return class responsible for setuping bitmap
+     * @return class responsible for setup of bitmap
      */
     @Bean
     public SetupImage createSetup(Image image, HistoryEntryRepository historyEntryRepository){
         return new SetupImage(image, historyEntryRepository);
     }
 
+    /**
+     * Class responsbile for setup of the bitmap
+     *
+     * @author Marcel Gawron
+     * @version 1.0
+     */
     public class SetupImage{
 
+        /**
+         * Construct class instance
+         * @param image reference to bitmap in memory wrapper class
+         * @param historyEntryRepository class responsible for handling storage of history entries in database
+         */
         public SetupImage(Image image, HistoryEntryRepository historyEntryRepository){
             reloadImageHistory(image, historyEntryRepository);
         }
 
+        /**
+         * Insert latest pixel changes into the in-memory bitmap
+         * @param image reference to bitmap in memory wrapper class
+         * @param historyEntryRepository class responsible for handling storage of history entries in database
+         */
         public void reloadImageHistory(Image image, HistoryEntryRepository historyEntryRepository) {
             List<HistoryEntry> historyEntries = historyEntryRepository.getLatestPixelChangeForEachPixel();
             for(var entry : historyEntries){
