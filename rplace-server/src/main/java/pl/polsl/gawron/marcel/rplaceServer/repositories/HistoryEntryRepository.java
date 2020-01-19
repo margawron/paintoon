7906fd5ecc02320e8874d01aeafe46ce1bb91fb1
margawron <max2.marcel@gmail.com>
2020-01-20 00:16:19 +0100

    Refactored repositories to use Spring Data JPA.

--- rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/repositories/HistoryEntryRepository.java
+++ rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/repositories/HistoryEntryRepository.java
@@ -1,17 +1,11 @@
 package pl.polsl.gawron.marcel.rplaceServer.repositories;
 
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import pl.polsl.gawron.marcel.rplaceData.models.HistoryEntry;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.util.ArrayList;
+
 import java.util.List;
 
 /**
@@ -20,134 +14,10 @@ import java.util.List;
  * @author Marcel Gawron
  * @version 1.0
  */
-@Component
-public class HistoryEntryRepository {
+@Repository
+public interface HistoryEntryRepository extends CrudRepository<HistoryEntry, Long> {
 
-    private JdbcTemplate template;
-    private UserRepository userRepository;
-
-    /**
-     * Creates table for history entries
-     *
-     * @param template       jdbc connection template - filled by spring context
-     * @param userRepository user database connection class - filled by spring context
-     */
-    public HistoryEntryRepository(JdbcTemplate template, UserRepository userRepository) {
-        this.template = template;
-        this.userRepository = userRepository;
-//        template.execute("DROP TABLE IF EXISTS historyEntries");
-//        template.execute("CREATE TABLE historyEntries(" +
-//                "id INTEGER AUTO_INCREMENT PRIMARY KEY," +
-//                "x INTEGER NOT NULL," +
-//                "y INTEGER NOT NULL," +
-//                "red INTEGER NOT NULL," +
-//                "green INTEGER NOT NULL," +
-//                "blue INTEGER NOT NULL," +
-//                "userId INTEGER NOT NULL ," +
-//                "timeOfModification INTEGER NOT NULL," +
-//                "FOREIGN KEY (userId) REFERENCES users(id) " +
-//                "ON DELETE SET NULL)");
-    }
-
-    /**
-     * Adds history entry to database
-     *
-     * @param entry history entry to be added
-     */
-    public void addHistoryEntry(HistoryEntry entry) {
-        template.update("INSERT INTO historyEntries(x,y,red,green,blue,userId,timeOfModification)" +
-                        " VALUES (?,?,?,?,?,?,?)",
-                entry.getX(), entry.getY(),
-                entry.getRedComponent(), entry.getGreenComponent(), entry.getGreenComponent(),
-                entry.getUserWhoModifiedPixel().getId(), entry.getTimeOfModification().atZone(ZoneId.systemDefault()).toEpochSecond());
-    }
-
-    /**
-     * Searches and returns individual history entry
-     *
-     * @param id history entry id
-     * @return history entry
-     */
-    public HistoryEntry getHistoryEntry(int id) {
-        HistoryEntry historyEntry = null;
-        try {
-            historyEntry = template.queryForObject("SELECT * FROM historyEntries WHERE id = ?", new Object[]{id}, new HistoryEntryRowMapper());
-        } catch (EmptyResultDataAccessException e) {
-            historyEntry = null;
-        }
-        return historyEntry;
-    }
-
-    /**
-     * Query for number of pixel changes of a user
-     *
-     * @param userId user id
-     * @return count of bitmap changes
-     */
-    public long getCountOfUserPixelChanges(long userId) {
-        Long userPixelChangesCount;
-        try {
-            userPixelChangesCount = template.queryForObject("SELECT COUNT(*) FROM historyEntries WHERE userId = ?", new Object[]{userId}, Long.class);
-        } catch (EmptyResultDataAccessException e) {
-            userPixelChangesCount = 0L;
-        }
-        if (userPixelChangesCount == null) {
-            userPixelChangesCount = 0L;
-        }
-        return userPixelChangesCount;
-    }
-
-    /**
-     * Gets all of the history entries in the database
-     *
-     * @return List of history entries
-     */
-    public List<HistoryEntry> getHistoryEntries() {
-        List<HistoryEntry> historyEntries = null;
-        try {
-            historyEntries = template.query("SELECT * FROM historyEntries", new HistoryEntryRowMapper());
-        } catch (EmptyResultDataAccessException e) {
-            historyEntries = null;
-        }
-        return historyEntries;
-    }
-
-    public List<HistoryEntry> getLatestPixelChangeForEachPixel() {
-        List<HistoryEntry> historyEntries = null;
-        try {
-            historyEntries = template.query("SELECT * from historyEntries h1 INNER JOIN " +
-                    "(SELECT x,y, MAX(id) as highest from historyEntries GROUP BY x, y ) AS h2 " +
-                    "ON h1.x = h2.x AND h1.y = h2.y AND h1.id = h2.highest", new HistoryEntryRowMapper());
-        } catch (EmptyResultDataAccessException e) {
-            historyEntries = new ArrayList(0);
-        }
-        return historyEntries;
-    }
-
-    /**
-     * Class implementing interface of a row mapper
-     * for HistoryEntry class
-     *
-     * @author Marcel Gawron
-     * @version 1.0
-     */
-    public final class HistoryEntryRowMapper implements RowMapper<HistoryEntry> {
-        @Override
-        public HistoryEntry mapRow(ResultSet resultSet, int i) throws SQLException {
-            HistoryEntry historyEntry = new HistoryEntry();
-            historyEntry.setId(resultSet.getLong("id"));
-            historyEntry.setX(resultSet.getInt("x"));
-            historyEntry.setY(resultSet.getInt("y"));
-            historyEntry.setRedComponent(resultSet.getInt("red"));
-            historyEntry.setGreenComponent(resultSet.getInt("green"));
-            historyEntry.setBlueComponent(resultSet.getInt("blue"));
-            historyEntry.setUserWhoModifiedPixel(userRepository.findUser(resultSet.getInt("userId")));
-            historyEntry.setTimeOfModification(LocalDateTime.ofEpochSecond(resultSet.getInt("timeOfModification"), 0, ZoneOffset.UTC));
-            return historyEntry;
-        }
-    }
-}
-/*
+    /*
     public List<HistoryEntry> getLatestPixelChangeForEachPixel() {
     public List<HistoryEntry> getHistoryEntries() {
     public long getCountOfUserPixelChanges(long userId){
@@ -155,3 +25,31 @@ public class HistoryEntryRepository {
     public void addHistoryEntry(HistoryEntry entry) {
 
 */
+
+    /**
+     * Get count of all user pixel changes
+     * @param userId user id
+     * @return count of pixel user changes
+     */
+    @Query("SELECT COUNT(h) FROM HistoryEntry h WHERE h.userWhoModifiedPixel.id = ?1")
+    Long getCountOfUserPixelChanges(long userId);
+
+    //SELECT * from historyEntries h1 INNER JOIN " +
+    //                    "(SELECT x,y, MAX(id) as highest from historyEntries GROUP BY x, y ) AS h2 " +
+    //                    "ON h1.x = h2.x AND h1.y = h2.y AND h1.id = h2.highest
+
+    /**
+     * Get list of last change for each pixel
+     * @return List of last changes for each pixel
+     */
+    @Query("Select h1, h2.x, h2.y, Max(h2.id) from HistoryEntry h1 inner join HistoryEntry h2 ON h1.x = h2.x WHERE h1.x = h2.x AND h1.y = h2.y AND h1.id = h2.id GROUP BY h2.x, h2.y")
+    List<HistoryEntry> getLatestPixelChangeForEachPixel();
+
+    /**
+     * Get all history pixel changes
+     * @return all history pixel changes
+     */
+    List<HistoryEntry> findAll();
+
+
+}
--- rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/repositories/UserRepository.java
+++ rplace-server/src/main/java/pl/polsl/gawron/marcel/rplaceServer/repositories/UserRepository.java
@@ -1,17 +1,9 @@
 package pl.polsl.gawron.marcel.rplaceServer.repositories;
 
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Component;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import pl.polsl.gawron.marcel.rplaceData.models.User;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.util.List;
 
 
@@ -21,122 +13,27 @@ import java.util.List;
  * @author Marcel Gawron
  * @version 1.0
  */
-@Component
-public class UserRepository {
-    private JdbcTemplate template;
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
 
     /**
-     * Default constructor
-     *
-     * @param template jdbc connection template - filled by spring context
+     * Get user by user name
+     * @param name user name
+     * @return user instance from database
      */
-    public UserRepository(JdbcTemplate template) {
-        this.template = template;
-//        template.execute("DROP TABLE IF EXISTS users");
-//        template.execute("CREATE TABLE users(" +
-//                "id INTEGER AUTO_INCREMENT PRIMARY KEY," +
-//                "name TEXT NOT NULL," +
-//                "password TEXT NOT NULL," +
-//                "tokenExpiryServerTime INTEGER," +
-//                "token TEXT)");
-    }
+    User findUserByName(String name);
 
     /**
-     * Adds user to database
-     *
-     * @param name     user name
-     * @param password user password
-     * @return true if user was added to database
+     * Get all users from database
+     * @return list of all users
      */
-    public boolean addUser(String name, String password) {
-        if (findUser(name) != null) {
-            return false;
-        } else {
-            template.update("INSERT INTO users(name, password) VALUES (?,?)", name, password);
-            return true;
-        }
-    }
+    List<User> findAll();
 
     /**
-     * Searches user in the database
-     *
-     * @param name username
-     * @return null if user was not found, user instance otherwise
+     * Save user to database
+     * @param object user object
+     * @return saved user object
      */
-    public User findUser(String name) {
-        User toReturn;
-        try {
-            toReturn = template.queryForObject("Select * from users where name = ?", new Object[]{name}, new UserRowMapper());
-        } catch (EmptyResultDataAccessException e) {
-            toReturn = null;
-        }
-        return toReturn;
-    }
-
-    /**
-     * Query for user with specific id
-     *
-     * @param id id of a user to query for
-     * @return User data from database
-     */
-    public User findUser(int id) {
-        User toReturn;
-        try {
-            toReturn = template.queryForObject("Select * from users where id = ?", new Object[]{id}, new UserRowMapper());
-        } catch (EmptyResultDataAccessException e) {
-            toReturn = null;
-        }
-        return toReturn;
-    }
-
-    /**
-     * Updates user entry in the database
-     *
-     * @param user user entry
-     * @return true if update passed, false otherwise
-     */
-    public boolean updateUser(User user) {
-        try {
-            template.update("UPDATE users SET name = ?, password = ?, token = ?, tokenExpiryServerTime = ? WHERE id = ?",
-                    user.getName(), user.getPassword(), user.getToken(), user.getTokenExpiryServerTime().atZone(ZoneId.systemDefault()).toEpochSecond(), user.getId());
-            return true;
-        } catch (DataAccessException e) {
-            return false;
-        }
-    }
-
-    /**
-     * Returns list of users from database
-     *
-     * @return list of users
-     */
-    public List<User> getAllUsers() {
-        List<User> toReturn = null;
-        try {
-            toReturn = template.query("SELECT * FROM users", new UserRowMapper());
-        } catch (EmptyResultDataAccessException ignored) {
-        }
-        return toReturn;
-    }
-
-    /**
-     * Class implementing interaface of a row mapper
-     * for User class
-     *
-     * @author Marcel Gawron
-     * @version 1.0
-     */
-    public final class UserRowMapper implements RowMapper<User> {
-        @Override
-        public User mapRow(ResultSet resultSet, int i) throws SQLException {
-            User user = new User();
-            user.setId(resultSet.getLong("id"));
-            user.setName(resultSet.getString("name"));
-            user.setPassword(resultSet.getString("password"));
-            user.setToken(resultSet.getString("token"));
-            user.setTokenExpiryServerTime(LocalDateTime.ofEpochSecond(resultSet.getInt("tokenExpiryServerTime"), 0, ZoneOffset.UTC));
-            return user;
-        }
-    }
+    User save(User object);
 }
 
